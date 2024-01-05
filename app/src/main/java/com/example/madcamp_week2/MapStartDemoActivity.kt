7c8728738f3d4d9ce59cb3423672bd3d58695000
Seongah Kim <kimsa0322@naver.com>
2024-01-05 14:06:58 +0900

    complete mapview

--- app/src/main/java/com/example/madcamp_week2/MapStartDemoActivity.kt
+++ app/src/main/java/com/example/madcamp_week2/MapStartDemoActivity.kt
@@ -2,6 +2,7 @@ package com.example.madcamp_week2
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.kakao.vectormap.KakaoMap
@@ -10,16 +11,21 @@ import com.kakao.vectormap.LatLng
 import com.kakao.vectormap.MapLifeCycleCallback
 import com.kakao.vectormap.MapView
 
-
-class MainActivity : AppCompatActivity() {
+class MapStartDemoActivity : AppCompatActivity() {
     private var mapView: MapView? = null
+    private var tvLat: TextView? = null
+    private var tvLng: TextView? = null
+    private var tvZoomLevel: TextView? = null
     private val startZoomLevel = 15
-    private val startPosition = LatLng.from(36.3721, 127.3604)
+    private val startPosition = LatLng.from(37.394660, 127.111182) // 판교역
 
     // MapReadyCallback 을 통해 지도가 정상적으로 시작된 후에 수신할 수 있다.
     private val readyCallback: KakaoMapReadyCallback = object : KakaoMapReadyCallback() {
         override fun onMapReady(kakaoMap: KakaoMap) {
             Toast.makeText(applicationContext, "Map Start!", Toast.LENGTH_SHORT).show()
+            tvLat!!.text = startPosition.getLatitude().toString()
+            tvLng!!.text = startPosition.getLongitude().toString()
+            tvZoomLevel!!.text = startZoomLevel.toString()
             Log.i(
                 "k3f", "startPosition: "
                         + kakaoMap.cameraPosition!!.position.toString()
@@ -66,10 +72,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val mapView = findViewById<MapView>(R.id.map_view)
+        setContentView(R.layout.activity_map_auth_start)
+        title = "Map Start"
+        tvLat = findViewById<TextView>(R.id.tvLat)
+        tvLng = findViewById<TextView>(R.id.tvLng)
+        tvZoomLevel = findViewById<TextView>(R.id.tvZoomLevel)
+        mapView = findViewById(R.id.map_view)
         mapView?.run { start(lifeCycleCallback, readyCallback) }
-
     }
 }
